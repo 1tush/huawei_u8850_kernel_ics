df523c6da6aa219abc28bdd8dfe5a9163354aa97
GriefNorth <grief.north@gmail.com>
2013-03-25 21:48:57 +0400

    remove dhd

--- drivers/net/wireless/dhd/.dhd.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_drivers/net/wireless/dhd/dhd.ko := /home/danil_e71/android-ndk/toolchains/arm-eabi-4.4.0/prebuilt/linux-x86/bin/arm-eabi-ld -EL -r  -T /home/danil_e71/android-ndk/toolchains/huawei_u8850_kernel_ics-master/scripts/module-common.lds --build-id -o drivers/net/wireless/dhd/dhd.ko drivers/net/wireless/dhd/dhd.o drivers/net/wireless/dhd/dhd.mod.o
